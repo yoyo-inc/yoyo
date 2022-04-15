4a79bfa848f7b1198e1ef33c3b9758173f3ccac4
ypli0629 <liyapeng0629@gmail.com>
2022-04-15 10:11:31 +0800

    feat: add logger middleware

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ypli0629/yoyo/middlewares"
+)
+
 func main() {
+	r := gin.New()
+	// middlewares
+	// logger
+	r.Use(middlewares.Logger())
+	// recovery
+	r.Use(gin.Recovery())
+
+	r.Run()
 }
