83d0d350186bc014bc87034ebd40c022793fc3c8
ypli0629 <liyapeng0629@gmail.com>
2022-04-18 17:58:23 +0800

    feat: add db

---
+++ common/db/db.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"github.com/ypli0629/yoyo/common/config"
+	"github.com/ypli0629/yoyo/common/logger"
+	"github.com/ypli0629/yoyo/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// Client provides the ability to manipulate database
+var Client *gorm.DB
+
+// Setup setups database connection
+func Setup() {
+	dns := config.Config.DB.DNS
+	var err error
+	Client, err = gorm.Open(mysql.Open(dns), &gorm.Config{})
+
+	if err != nil {
+		logger.Panicf("Failed to connect database: %s", err)
+	} else {
+		logger.Info("Connect database successfully")
+	}
+
+	logger.Info("Begin to automigrate")
+	automigrate()
+}
+
+func automigrate() {
+	Client.
+		Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4").
+		AutoMigrate(
+			&models.User{},
+		)
+}
