b054ce58f69894a8ef1c5e92898374554dff18b6
ypli0629 <liyapeng0629@gmail.com>
2022-04-19 14:55:01 +0800

    chore: add dependencies

---
+++ scripts/dependencies.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo docker rm -f yoyo-mysql
+sudo docker run --name yoyo-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_DATABASE=yoyo -e MYSQL_USER=yoyo -e MYSQL_PASSWORD=yoyo -d mysql:8.0.28
