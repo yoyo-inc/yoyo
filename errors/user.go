ee0af9345ca037dc8a15319698b2829c27dbdc42
ypli0629 <liyapeng0629@gmail.com>
2022-04-26 10:22:57 +0800

    feat: add ErrCreateUser

--- errors/user.go
+++ errors/user.go
@@ -6,4 +6,5 @@ var (
 	FailedToLogin         = core.NewBusinessError("100", "登录失败")
 	ErrUsernameNotExists  = core.NewBusinessError("101", "账号不存在")
 	ErrUsernameOrPassword = core.NewBusinessError("102", "账号或密码错误")
+	ErrCreateUser         = core.NewBusinessError("103", "用户创建失败")
 )
