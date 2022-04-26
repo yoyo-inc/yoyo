e8137171ba4ba03ba58847721420535d10eacf44
ypli0629 <liyapeng0629@gmail.com>
2022-04-26 10:22:16 +0800

    feat: add parameter error

--- core/error.go
+++ core/error.go
@@ -1,5 +1,22 @@
 package core
 
+// ParameterError represents parameter error
+type ParameterError struct {
+	Message string
+}
+
+func (e ParameterError) Error() string {
+	return e.Message
+}
+
+// NewParameterError returns parameter error
+func NewParameterError(message string) ParameterError {
+	return ParameterError{
+		Message: message,
+	}
+}
+
+// BusinessError represents bussiness error
 type BusinessError struct {
 	Code    string
 	Message string
@@ -9,7 +26,7 @@ func (e BusinessError) Error() string {
 	return e.Message
 }
 
-// NewBusinessError returns runtime error
+// NewBusinessError returns business error
 func NewBusinessError(code string, message string) BusinessError {
 	return BusinessError{
 		Code:    code,
