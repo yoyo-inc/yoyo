031660b74490ee398ea2cbaeb64c12cfe5967fc2
ypli0629 <liyapeng0629@gmail.com>
2022-07-05 11:32:11 +0800

    feat: add Organization model

---
+++ models/organization.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"github.com/ypli0629/yoyo/common/db"
+	"github.com/ypli0629/yoyo/core"
+)
+
+type Organization struct {
+	core.Model
+	Name     string `json:"name" gorm:"size:255;comment:组织名称"`
+	ParentID string `json:"parentId"`
+}
+
+func init() {
+	db.AddAutoMigrateModel(&Organization{})
+}
