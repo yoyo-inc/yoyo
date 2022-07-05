e5c527a9f1fa66d6b9066b8d196c90c9c0b2995a
ypli0629 <liyapeng0629@gmail.com>
2022-07-05 11:32:21 +0800

    feat: add Permission model

---
+++ models/permission.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"github.com/ypli0629/yoyo/common/db"
+	"github.com/ypli0629/yoyo/core"
+)
+
+type Permission struct {
+	core.Model
+	Name     string `json:"name" gorm:"size:255;unique;not null;comment:权限名称"`
+	ParentID string `json:"parent_id" gorm:"size:32;comment:父级权限"`
+}
+
+func init() {
+	db.AddAutoMigrateModel(&Permission{})
+}
