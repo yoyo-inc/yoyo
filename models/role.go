d5fa1c3de6402c6baff885eebf8c256aafefcce4
ypli0629 <liyapeng0629@gmail.com>
2022-07-05 17:30:28 +0800

    feat: add role permissions

--- models/role.go
+++ models/role.go
@@ -8,8 +8,9 @@ import (
 // Role represents user role
 type Role struct {
 	core.Model
-	Name   string `json:"name" gorm:"size:256;unique;not null;comment:角色名" binding:"required,max=250"`
-	Remark string `json:"remark" gorm:"size:256;comment:备注"`
+	Name        string       `json:"name" gorm:"size:256;unique;not null;comment:角色名" binding:"required,max=250"`
+	Remark      string       `json:"remark" gorm:"size:256;comment:备注"`
+	Permissions []Permission `json:"permissions" gorm:"many2many:role_permissions"`
 }
 
 func init() {
