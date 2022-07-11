ed9d1cf7c726eedfae864a1a23dbf25fdda8cd23
ypli0629 <liyapeng0629@gmail.com>
2022-07-11 16:24:26 +0800

    feat: add update user

--- errors/user.go
+++ errors/user.go
@@ -8,4 +8,6 @@ var (
 	ErrUsernameOrPassword = core.NewBusinessError("102", "账号或密码错误")
 	ErrCreateUser         = core.NewBusinessError("103", "用户创建失败")
 	ErrQueryUser          = core.NewBusinessError("104", "用户查询失败")
+	ErrDeleteUser         = core.NewBusinessError("105", "用户删除失败")
+	ErrUpdateUser         = core.NewBusinessError("106", "用户更新失败")
 )
