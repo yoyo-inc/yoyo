728976b75e7df8e07bad85351f6b08c6e6f32ad5
ypli0629 <liyapeng0629@gmail.com>
2022-07-12 14:39:49 +0800

    feat: creatTime and modifyTime use LocalTime datatype

--- core/model.go
+++ core/model.go
@@ -1,20 +1,19 @@
 package core
 
 import (
-	"strings"
-	"time"
-
 	"github.com/google/uuid"
+	"github.com/ypli0629/yoyo/common/datatypes"
 	"gorm.io/gorm"
+	"strings"
 )
 
 type Model struct {
 	// 主键
 	ID string `json:"id" gorm:"primarykey;size:32;comment:主键"`
 	// 创建时间
-	CreateTime *time.Time `json:"createTime" gorm:"autoCreateTime;comment:创建时间"`
+	CreateTime *datatypes.LocalTime `json:"createTime" gorm:"autoCreateTime;comment:创建时间"`
 	// 更新时间
-	ModifyTime *time.Time `json:"modifyTime" gorm:"autoUpdateTime;comment:更新时间"`
+	ModifyTime *datatypes.LocalTime `json:"modifyTime" gorm:"autoUpdateTime;comment:更新时间"`
 }
 
 func (m *Model) BeforeCreate(tx *gorm.DB) (err error) {
