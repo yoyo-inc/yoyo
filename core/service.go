1d9998cb1781b61e0bad587321b5cd8efc901d8c
ypli0629 <liyapeng0629@gmail.com>
2022-07-12 09:51:07 +0800

    feat: add paginator

--- core/service.go
+++ core/service.go
@@ -1,1 +1,29 @@
 package core
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"strconv"
+)
+
+func Paginator(c *gin.Context) func(db *gorm.DB) *gorm.DB {
+	page := getParam(c, "page", 1)
+	pageSize := getParam(c, "pageSize", 10)
+
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Limit(pageSize).Offset((page - 1) * pageSize)
+	}
+}
+
+func getParam(c *gin.Context, key string, defaultValue int) int {
+	var result = defaultValue
+	if param, exists := c.GetQuery(key); exists {
+		var err error
+		result, err = strconv.Atoi(param)
+		if err != nil {
+			result = defaultValue
+		}
+	}
+
+	return result
+}
