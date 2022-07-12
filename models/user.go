ef42d92cd9634887f71995237cd2234804f31631
ypli0629 <liyapeng0629@gmail.com>
2022-07-12 11:37:30 +0800

    feat: add comment

--- models/user.go
+++ models/user.go
@@ -12,14 +12,16 @@ type User struct {
 	core.Model
 	// 账户名
 	Username string `json:"username" gorm:"size:15;unique;not null;index;comment:账户名" binding:"required,min=0,max=15"`
+	// 昵称
+	Nickname string `json:"nickname" gorm:"size:255;index;comment:昵称" binding:"max=255"`
 	// 密码
-	Password string `json:"password,omitempty" gorm:"size:256;not null;comment:密码"`
+	Password string `json:"password,omitempty" gorm:"size:255;not null;comment:密码" binding:"max=255"`
 	// 邮箱
-	Email string `json:"email" gorm:"size:256;comment:邮箱" binding:"email"`
+	Email string `json:"email" gorm:"size:255;comment:邮箱" binding:"email,max=255"`
 	// 手机号
 	Phone string `json:"phone" gorm:"size:11;comment:手机号" binding:"len=11"`
 	// 头像
-	Avatar string `json:"avatar" gorm:"size:256;comment:头像" binding:"max=250"`
+	Avatar string `json:"avatar" gorm:"size:255;comment:头像" binding:"max=255"`
 	// 性别 0: 男 1: 女
 	Sex int `json:"sex" gorm:"comment:性别"`
 	// 年龄
@@ -50,6 +52,8 @@ func (user *User) BeforeUpdate(tx *gorm.DB) (err error) {
 }
 
 type QueryUser struct {
+	// 账户名
 	Username string `form:"username"`
-	Phone    string `form:"phone"`
+	// 手机号
+	Phone string `form:"phone"`
 }
