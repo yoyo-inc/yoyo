10e324a4cfab630f40619adf7a68da9096bbba69
ypli0629 <liyapeng0629@gmail.com>
2022-07-12 10:47:37 +0800

    feat: add comment

--- models/permission.go
+++ models/permission.go
@@ -7,8 +7,9 @@ import (
 
 type Permission struct {
 	core.Model
-	// 权限名
-	Name     string `json:"name" gorm:"size:255;unique;not null;comment:权限名称"`
+	// 权限名称
+	Name string `json:"name" gorm:"size:255;unique;not null;comment:权限名称"`
+	// 父级权限
 	ParentID string `json:"parentID" gorm:"size:32;comment:父级权限"`
 }
 
