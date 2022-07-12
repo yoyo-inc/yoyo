e8d9bd9b212ff17749b04683d7f8624ca7efef87
ypli0629 <liyapeng0629@gmail.com>
2022-07-12 09:51:35 +0800

    feat: add role error

---
+++ errors/role.go
@@ -0,0 +1,10 @@
+package errors
+
+import "github.com/ypli0629/yoyo/core"
+
+var (
+	ErrCreateRole = core.NewBusinessError("200", "角色创建失败")
+	ErrUpdateRole = core.NewBusinessError("201", "角色更新失败")
+	ErrDeleteRole = core.NewBusinessError("202", "角色删除失败")
+	ErrQueryRole  = core.NewBusinessError("203", "角色查询失败")
+)
