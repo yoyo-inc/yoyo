29364095d41b90f84815351e0c73a1ea778e075a
ypli0629 <liyapeng0629@gmail.com>
2022-07-12 10:13:25 +0800

    feat: change json tag name

--- models/permission.go
+++ models/permission.go
@@ -9,7 +9,7 @@ type Permission struct {
 	core.Model
 	// 权限名
 	Name     string `json:"name" gorm:"size:255;unique;not null;comment:权限名称"`
-	ParentID string `json:"parent_id" gorm:"size:32;comment:父级权限"`
+	ParentID string `json:"parentID" gorm:"size:32;comment:父级权限"`
 }
 
 func init() {
