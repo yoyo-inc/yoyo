6b41f7d2ba4ca6df8fca76013ad6e0a052c849f5
ypli0629 <liyapeng0629@gmail.com>
2022-07-15 14:23:59 +0800

    feat: log support level

--- common/logger/logger.go
+++ common/logger/logger.go
@@ -4,6 +4,7 @@ import (
 	"github.com/sirupsen/logrus"
 	logrusSyslog "github.com/sirupsen/logrus/hooks/syslog"
 	"log/syslog"
+	"strings"
 )
 
 var logger logrus.Logger
@@ -17,6 +18,7 @@ type SyslogOption struct {
 // Options contains logger options
 type Options struct {
 	Service string
+	Level   string
 	Syslog  *SyslogOption
 }
 
@@ -27,6 +29,26 @@ func Setup(options Options) {
 	service := "default"
 	if options.Service != "" {
 		service = options.Service
+	}
+	if options.Level != "" {
+		level := logrus.InfoLevel
+		switch strings.ToLower(options.Level) {
+		case "panic":
+			level = logrus.PanicLevel
+		case "fatal":
+			level = logrus.FatalLevel
+		case "error":
+			level = logrus.ErrorLevel
+		case "warn":
+			level = logrus.WarnLevel
+		case "info":
+		case "debug":
+			level = logrus.DebugLevel
+		case "trace":
+			level = logrus.TraceLevel
+		}
+
+		logger.SetLevel(level)
 	}
 
 	logger.SetFormatter(&TextFormatter{
@@ -48,44 +70,74 @@ func SetupSyslog(service string, option *SyslogOption) {
 	}
 }
 
-// Info logs INFO_LEVEL message
-func Info(args ...interface{}) {
-	logger.Infoln(args...)
-}
-
-// Infof logs INFO_LEVEL message by format
-func Infof(format string, args ...interface{}) {
-	logger.Infof(format, args...)
-}
-
-// Warn logs WARN_LEVEL message
-func Warn(args ...interface{}) {
-	logger.Warnln(args...)
-}
-
-// Warnf logs WARN_LEVEL message
-func Warnf(format string, args ...interface{}) {
-	logger.Warnf(format, args...)
-}
-
-// Error logs ERROR_LEVEL message
-func Error(args ...interface{}) {
-	logger.Errorln(args...)
-}
-
-// Errorf logs ERROR_LEVEL message by format
-func Errorf(format string, args ...interface{}) {
-	logger.Errorf(format, args...)
-}
-
-// Panic logs PANIC_LEVEL message
+// Panic logs PanicLevel message
 func Panic(args ...interface{}) {
 	logger.Panicln(args...)
 }
 
-// Panicf logs PANIC_LEVEL_LEVEL message by format
+// Panicf logs PanicLevel message by format
 func Panicf(format string, args ...interface{}) {
 	logger.Panicf(format, args...)
+}
+
+// Fatal log FatalLevel message
+func Fatal(args ...interface{}) {
+	logger.Fatalln(args...)
+}
+
+// Fatalf log FatalLevel message by format
+func Fatalf(format string, args ...interface{}) {
+	logger.Fatalf(format, args...)
+}
+
+// Error logs ErrorLevel message
+func Error(args ...interface{}) {
+	logger.Errorln(args...)
+}
+
+// Errorf logs ErrorLevel message by format
+func Errorf(format string, args ...interface{}) {
+	logger.Errorf(format, args...)
+}
+
+// Warn logs WarnLevel message
+func Warn(args ...interface{}) {
+	logger.Warnln(args...)
+}
+
+// Warnf logs WarnLevel message
+func Warnf(format string, args ...interface{}) {
+	logger.Warnf(format, args...)
+}
+
+// Info logs InfoLevel message
+func Info(args ...interface{}) {
+	logger.Infoln(args...)
+}
+
+// Infof logs InfoLevel message by format
+func Infof(format string, args ...interface{}) {
+	logger.Infof(format, args...)
+}
+
+// Debug logs DebugLevel message
+func Debug(args ...interface{}) {
+	logger.Debugln(args...)
+}
+
+// Debugf logs DebugLevel message by format
+func Debugf(format string, args ...interface{}) {
+	logger.Debugf(format, args...)
+}
+
+// Trace logs TraceLevel message
+func Trace(args ...interface{}) {
+	logger.Traceln(args...)
+}
+
+// Tracef logs TraceLevel message by format
+func Tracef(format string, args ...interface{}) {
+	logger.Tracef(format, args...)
 }
 
 func GetLogger() *logrus.Logger {
