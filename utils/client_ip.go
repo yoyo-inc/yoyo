4cfb81387632600d40bff314ed9dc428770e9664
ypli0629 <liyapeng0629@gmail.com>
2022-09-23 23:44:18 +0800

    feat: add get client ip

--- utils/client_ip.go
+++ utils/client_ip.go
@@ -1,1 +1,14 @@
 package utils
+
+import "github.com/gin-gonic/gin"
+
+func GetClientIP(c *gin.Context) string {
+	var ip string
+	xRealIP := c.GetHeader("X-Real-IP")
+	if xRealIP != "" {
+		ip = xRealIP
+	} else {
+		ip = c.ClientIP()
+	}
+	return ip
+}
