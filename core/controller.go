a9360a07bb5dca4ee01b4c3e0ecf71a288c3058b
ypli0629 <liyapeng0629@gmail.com>
2022-09-23 23:35:07 +0800

    feat: remove no security

--- core/controller.go
+++ core/controller.go
@@ -1,11 +1,17 @@
 package core
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/yoyo-inc/yoyo/common/db"
+	"gorm.io/gorm"
+)
 
 type Controller interface {
 	Setup(r *gin.RouterGroup)
 }
 
-type NoSecurityController interface {
-	SetupNoSecurity(r *gin.RouterGroup)
+func GetPaginatedQuery(model interface{}) [2]*gorm.DB {
+	modelQuery := db.Client.Model(model)
+	totalQuery := db.Client.Model(model)
+	return [2]*gorm.DB{modelQuery, totalQuery}
 }
