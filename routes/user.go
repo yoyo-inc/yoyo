c274239343fac48e49551da728181b747af25f88
ypli0629 <liyapeng0629@gmail.com>
2022-09-26 16:49:53 +0800

    feat: add audit_log

--- routes/user.go
+++ routes/user.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"errors"
+
 	"github.com/gin-gonic/gin"
 	jwt "github.com/yoyo-inc/gin-jwt/v3"
 	"github.com/yoyo-inc/yoyo/common/db"
@@ -9,11 +11,13 @@ import (
 	"github.com/yoyo-inc/yoyo/errs"
 	"github.com/yoyo-inc/yoyo/middlewares"
 	"github.com/yoyo-inc/yoyo/models"
+	"github.com/yoyo-inc/yoyo/services/audit_log"
+	"gorm.io/gorm"
 )
 
 type userController struct{}
 
-// RetrieveUser
+// Retrieve
 // @Summary  查询用户列表
 // @Tags     user
 // @Produce  json
@@ -21,7 +25,7 @@ type userController struct{}
 // @Param    query  query    models.Pagination  false  "参数"
 // @Success  200    {object}  core.Response{data=core.PaginatedData{list=[]models.User}}
 // @Router   /user [get]
-func (*userController) RetrieveUser(c *gin.Context) {
+func (*userController) Retrieve(c *gin.Context) {
 	var query models.QueryUser
 	if err := c.ShouldBindQuery(&query); err != nil {
 		c.Error(core.NewParameterError(err.Error()))
@@ -34,6 +38,7 @@ func (*userController) RetrieveUser(c *gin.Context) {
 		modelQuery.Where("username like %?%", query.Username)
 		query.Username = ""
 	}
+
 	if result := modelQuery.Where(&query).Scopes(core.Paginator(c)).Find(&users); result.Error != nil {
 		logger.Error(result.Error)
 		c.Error(errs.ErrQueryUser)
@@ -67,8 +72,11 @@ func (*userController) CreateUser(c *gin.Context) {
 	if result := db.Client.Create(&query); result.Error != nil {
 		logger.Error(result.Error)
 		c.Error(errs.ErrCreateUser)
+		audit_log.Fail(c, "用户", "新增", query.Username)
 		return
 	}
+
+	audit_log.Success(c, "用户", "新增", query.Username)
 	core.OK(c, query)
 }
 
@@ -90,8 +98,10 @@ func (*userController) UpdateUser(c *gin.Context) {
 	if result := db.Client.Save(&query); result.Error != nil {
 		logger.Error(result.Error)
 		c.Error(errs.ErrUpdateUser)
+		audit_log.Fail(c, "用户", "更新", query.Username)
 		return
 	}
+	audit_log.Success(c, "用户", "更新", query.Username)
 	core.OK(c, true)
 }
 
@@ -105,11 +115,23 @@ func (*userController) UpdateUser(c *gin.Context) {
 func (*userController) DeleteUser(c *gin.Context) {
 	userID := c.Param("userID")
 
-	if result := db.Client.Delete(&models.User{}, userID); result.Error != nil {
+	var existUser models.User
+	if res := db.Client.Model(&models.User{Model: core.Model{ID: userID}}).First(&existUser); res.Error != nil {
+		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			logger.Error(res.Error)
+			c.Error(errs.ErrUsernameNotExists)
+			audit_log.Fail(c, "用户", "删除", existUser.Username)
+			return
+		}
+	}
+
+	if result := db.Client.Delete(&models.User{}, "id = ?", userID); result.Error != nil {
 		logger.Error(result.Error)
 		c.Error(errs.ErrDeleteUser)
+		audit_log.Fail(c, "用户", "删除", existUser.Username)
 		return
 	}
+	audit_log.Success(c, "用户", "删除", existUser.Username)
 	core.OK(c, true)
 }
 
@@ -134,7 +156,7 @@ func (*userController) RetrieveCurrentUser(c *gin.Context) {
 }
 
 func (user *userController) Setup(r *gin.RouterGroup) {
-	r.GET("/user", user.RetrieveUser).
+	r.GET("/user", user.Retrieve).
 		POST("/user", user.CreateUser).
 		PUT("/user", user.UpdateUser).
 		DELETE("/user/:userID", user.DeleteUser).
