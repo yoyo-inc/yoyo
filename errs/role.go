3227be80aaf02de8745ab960dab68e2d7f909b7d
ypli0629 <liyapeng0629@gmail.com>
2022-10-13 18:39:28 +0800

    feat: add ErrNotExistRole

--- errs/role.go
+++ errs/role.go
@@ -7,4 +7,5 @@ var (
 	ErrUpdateRole = core.NewBusinessError("201", "角色更新失败")
 	ErrDeleteRole = core.NewBusinessError("202", "角色删除失败")
 	ErrQueryRole  = core.NewBusinessError("203", "角色查询失败")
+	ErrNotExistRole = core.NewBusinessError("204", "角色不存在")
 )
