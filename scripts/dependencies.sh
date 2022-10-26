51aa51a4f33955d7e70ceb9b7e891d59b3923682
ypli0629 <liyapeng0629@gmail.com>
2022-10-26 11:46:55 +0800

    chore(scripts): remove sudo

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo docker rm -f yoyo-mysql
-sudo docker run --name yoyo-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_DATABASE=yoyo -e MYSQL_USER=yoyo -e MYSQL_PASSWORD=yoyo -d mysql:8.0.28
+docker rm -f yoyo-mysql
+docker run --name yoyo-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_DATABASE=yoyo -e MYSQL_USER=yoyo -e MYSQL_PASSWORD=yoyo -d mysql:8.0.28
