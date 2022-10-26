da4f996c23edea5b316dae9edbaf7ee23934ea23
ypli0629 <liyapeng0629@gmail.com>
2022-10-26 11:47:47 +0800

    feat(errs): add role

--- errs/role.go
+++ errs/role.go
@@ -3,9 +3,10 @@ package errs
 import "github.com/yoyo-inc/yoyo/core"
 
 var (
-	ErrCreateRole = core.NewBusinessError("200", "角色创建失败")
-	ErrUpdateRole = core.NewBusinessError("201", "角色更新失败")
-	ErrDeleteRole = core.NewBusinessError("202", "角色删除失败")
-	ErrQueryRole  = core.NewBusinessError("203", "角色查询失败")
+	ErrCreateRole   = core.NewBusinessError("200", "角色创建失败")
+	ErrUpdateRole   = core.NewBusinessError("201", "角色更新失败")
+	ErrDeleteRole   = core.NewBusinessError("202", "角色删除失败")
+	ErrQueryRole    = core.NewBusinessError("203", "角色查询失败")
 	ErrNotExistRole = core.NewBusinessError("204", "角色不存在")
+	ErrExistRole    = core.NewBusinessError("205", "角色已存在")
 )
