f00994ad85d1d33435b29fceb1893e60937f0d23
ypli0629 <liyapeng0629@gmail.com>
2022-11-06 20:16:56 +0800

    feat: autoIncrement primaryKey by default

--- core/model.go
+++ core/model.go
@@ -1,13 +1,23 @@
 package core
 
 import (
+	"strings"
+
 	"github.com/google/uuid"
 	"github.com/yoyo-inc/yoyo/common/datatypes"
 	"gorm.io/gorm"
-	"strings"
 )
 
 type Model struct {
+	// 主键
+	ID int `json:"id" gorm:"primarykey;autoIncreatment;comment:主键"`
+	// 创建时间
+	CreateTime *datatypes.LocalTime `json:"createTime" gorm:"type:timestamp;default:current_timestamp;<-:create;comment:创建时间"`
+	// 更新时间
+	ModifyTime *datatypes.LocalTime `json:"modifyTime" gorm:"type:timestamp;default:current_timestamp on update current_timestamp;comment:更新时间"`
+}
+
+type SModel struct {
 	// 主键
 	ID string `json:"id" gorm:"primarykey;size:32;comment:主键"`
 	// 创建时间
@@ -16,7 +26,7 @@ type Model struct {
 	ModifyTime *datatypes.LocalTime `json:"modifyTime" gorm:"type:timestamp;default:current_timestamp on update current_timestamp;comment:更新时间"`
 }
 
-func (m *Model) BeforeCreate(tx *gorm.DB) (err error) {
+func (m *SModel) BeforeCreate(tx *gorm.DB) (err error) {
 	if m.ID == "" {
 		id := uuid.NewString()
 		id = strings.ReplaceAll(id, "-", "")
@@ -28,7 +38,7 @@ func (m *Model) BeforeCreate(tx *gorm.DB) (err error) {
 	return
 }
 
-func (m *Model) BeforeUpdate(tx *gorm.DB) (err error) {
+func (m *SModel) BeforeUpdate(tx *gorm.DB) (err error) {
 	m.CreateTime = nil
 	m.ModifyTime = nil
 	return
