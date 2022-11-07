94587f6d1f41b54c168a2e27c0dea9e58b2dc317
ypli0629 <liyapeng0629@gmail.com>
2022-11-07 22:26:58 +0800

    feat: optimize

--- routes/user.go
+++ routes/user.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/duke-git/lancet/v2/slice"
 	"github.com/gin-gonic/gin"
@@ -14,18 +15,20 @@ import (
 	"github.com/yoyo-inc/yoyo/models"
 	"github.com/yoyo-inc/yoyo/services/audit_log"
 	"github.com/yoyo-inc/yoyo/vo"
+	"gorm.io/gorm/clause"
 )
 
 type userController struct{}
 
 // QueryUsers
-// @Summary  查询用户列表
-// @Tags     user
-// @Produce  json
-// @Param    query  query     vo.QueryUserVO       false  "参数"
-// @Param    query  query     models.Pagination  false  "参数"
-// @Success  200    {object}  core.Response{data=core.PaginatedData{list=[]models.User}}
-// @Router   /users [get]
+// @Summary 查询用户列表
+// @Tags    user
+// @Produce json
+// @Param   query query    vo.QueryUserVO    false "参数"
+// @Param   query query    models.Pagination false "参数"
+// @Success 200   {object} core.Response{data=core.PaginatedData{list=[]models.User}}
+// @Security JWT
+// @Router  /users [get]
 func (*userController) QueryUsers(c *gin.Context) {
 	var query vo.QueryUserVO
 	if err := c.ShouldBindQuery(&query); err != nil {
@@ -42,7 +45,7 @@ func (*userController) QueryUsers(c *gin.Context) {
 	}
 
 	var users []models.User
-	if res := queries[0].Scopes(core.Paginator(c)).Where(query).Find(&users); res.Error != nil {
+	if res := queries[0].Preload("Roles").Scopes(core.Paginator(c)).Where(query).Find(&users); res.Error != nil {
 		logger.Error(res.Error)
 		c.Error(errs.ErrQueryUser)
 		return
@@ -59,13 +62,14 @@ func (*userController) QueryUsers(c *gin.Context) {
 }
 
 // CreateUser
-// @Summary  创建用户
-// @Tags     user
-// @Accept   json
-// @Produce  json
-// @Param    query  body      vo.UserVO  true  "用户信息"
-// @Success  200    {object}  core.Response{data=models.User}
-// @Router   /user [post]
+// @Summary 创建用户
+// @Tags    user
+// @Accept  json
+// @Produce json
+// @Param   query body     vo.UserVO true "用户信息"
+// @Success 200   {object} core.Response{data=models.User}
+// @Security JWT
+// @Router  /user [post]
 func (*userController) CreateUser(c *gin.Context) {
 	var query vo.UserVO
 	if err := c.ShouldBindJSON(&query); err != nil {
@@ -73,25 +77,45 @@ func (*userController) CreateUser(c *gin.Context) {
 		return
 	}
 
-	if res := db.Client.Create(&query); res.Error != nil {
+	// judge whether user exists
+	var count int64
+	if res := db.Client.Model(&models.User{}).Where("username = ?", query.Username).Count(&count); res.Error != nil {
 		logger.Error(res.Error)
 		c.Error(errs.ErrCreateUser)
-		audit_log.Fail(c, "用户", "新增", query.Username)
+		audit_log.Fail(c, "用户", "新增", "用户名："+query.Username)
+		return
+	}
+	if count > 0 {
+		c.Error(errs.ErrExistUser)
+		audit_log.Fail(c, "用户", "新增", "用户已存在，用户名："+query.Username)
 		return
 	}
 
-	audit_log.Success(c, "用户", "新增", query.Username)
+	var user = query.User
+	user.Roles = slice.Map(query.Roles, func(_ int, roleID int) models.Role {
+		return models.Role{Model: core.Model{ID: roleID}}
+	})
+
+	if res := db.Client.Create(&user); res.Error != nil {
+		logger.Error(res.Error)
+		c.Error(errs.ErrCreateUser)
+		audit_log.Fail(c, "用户", "新增", "用户名："+query.Username)
+		return
+	}
+
+	audit_log.Success(c, "用户", "新增", "用户名："+query.Username)
 	core.OK(c, query)
 }
 
 // UpdateUser
-// @Summary  更新用户
-// @Tags     user
-// @Accept   json
-// @Produce  json
-// @Param    query  body      vo.UserVO  true  "用户信息"
-// @Success  200    {object}  core.Response{data=bool}
-// @Router   /user [put]
+// @Summary 更新用户
+// @Tags    user
+// @Accept  json
+// @Produce json
+// @Param   query body     vo.UserVO true "用户信息"
+// @Success 200   {object} core.Response{data=bool}
+// @Security JWT
+// @Router  /user [put]
 func (*userController) UpdateUser(c *gin.Context) {
 	var query vo.UserVO
 	if err := c.ShouldBindJSON(&query); err != nil {
@@ -102,67 +126,70 @@ func (*userController) UpdateUser(c *gin.Context) {
 	if res := db.Client.Save(&query.User); res.Error != nil {
 		logger.Error(res.Error)
 		c.Error(errs.ErrUpdateUser)
-		audit_log.Fail(c, "用户", "更新", query.Username)
+		audit_log.Fail(c, "用户", "更新", "用户名："+query.Username)
 		return
 	}
 
 	// update user associations
 	// update user roles
 	if query.Roles != nil {
-		roles := slice.Map(query.Roles, func(_ int, roleID string) models.Role {
+		roles := slice.Map(query.Roles, func(_ int, roleID int) models.Role {
 			return models.Role{Model: core.Model{ID: roleID}}
 		})
 		if err := db.Client.Model(&models.User{}).Association("Roles").Replace(roles); err != nil {
 			logger.Error(err)
 			c.Error(errs.ErrUpdateUser)
-			audit_log.Fail(c, "用户", "更新", query.Username)
+			audit_log.Fail(c, "用户", "更新", "用户名："+query.Username)
 		}
 	}
-	audit_log.Success(c, "用户", "更新", query.Username)
+	audit_log.Success(c, "用户", "更新", "用户名："+query.Username)
 	core.OK(c, true)
 }
 
 // DeleteUser
-// @Summary  删除用户
-// @Tags     user
-// @Produce  json
-// @Param    userID  path      string  true  "用户ID"
-// @Success  200     {object}  core.Response{data=bool}
-// @Router   /user/{userID} [delete]
+// @Summary 删除用户
+// @Tags    user
+// @Produce json
+// @Param   userID path     string true "用户ID"
+// @Success 200    {object} core.Response{data=bool}
+// @Security JWT
+// @Router  /user/{userID} [delete]
 func (*userController) DeleteUser(c *gin.Context) {
 	userID := c.Param("userID")
+	id, _ := strconv.Atoi(userID)
 
 	var existUser models.User
-	if res := db.Client.Model(&models.User{Model: core.Model{ID: userID}}).First(&existUser); res.Error != nil {
+	if res := db.Client.Model(&models.User{Model: core.Model{ID: id}}).First(&existUser); res.Error != nil {
 		logger.Error(res.Error)
 		c.Error(errs.ErrUsernameNotExists)
-		audit_log.Fail(c, "用户", "删除", fmt.Sprintf("用户ID(%s}不存在", userID))
+		audit_log.Fail(c, "用户", "删除", fmt.Sprintf("用户ID(%s)不存在", userID))
 		return
 	}
 
-	if res := db.Client.Delete(&models.User{}, "id = ?", userID); res.Error != nil {
+	if res := db.Client.Select(clause.Associations).Delete(&models.User{Model: core.Model{ID: id}}); res.Error != nil {
 		logger.Error(res.Error)
 		c.Error(errs.ErrDeleteUser)
-		audit_log.Fail(c, "用户", "删除", existUser.Username)
+		audit_log.Fail(c, "用户", "删除", "用户名："+existUser.Username)
 		return
 	}
-	audit_log.Success(c, "用户", "删除", existUser.Username)
+	audit_log.Success(c, "用户", "删除", "用户名："+existUser.Username)
 	core.OK(c, true)
 }
 
 // QueryCurrentUser
-// @Summary  查询当前用户信息
-// @Tags     user
-// @Accept   json
-// @Produce  json
-// @Success  200  {object}  core.Response{data=models.User}
-// @Router   /user/current [get]
+// @Summary 查询当前用户信息
+// @Tags    user
+// @Accept  json
+// @Produce json
+// @Success 200 {object} core.Response{data=models.User}
+// @Security JWT
+// @Router  /user/current [get]
 func (*userController) QueryCurrentUser(c *gin.Context) {
 	claims := jwt.ExtractClaims(c)
 	userID, _ := claims[middlewares.IdentityKey].(string)
 
 	var user models.User
-	if result := db.Client.Omit("password").Find(&user, userID); result.Error != nil {
+	if result := db.Client.Omit("password").Preload("Roles").Find(&user, userID); result.Error != nil {
 		logger.Error(result.Error)
 		c.Error(errs.ErrQueryCurrentUser)
 		return
@@ -170,10 +197,39 @@ func (*userController) QueryCurrentUser(c *gin.Context) {
 	core.OK(c, user)
 }
 
+// QueryCurrentUserPermissions
+// @Summary 查询当前用户权限信息
+// @Tags    user
+// @Accept  json
+// @Produce json
+// @Success 200 {object} core.Response{data=array,string}
+// @Security JWT
+// @Router  /user/current/permissions [get]
+func (*userController) QueryCurrentUserPermissions(c *gin.Context) {
+	claims := jwt.ExtractClaims(c)
+	userID, _ := claims[middlewares.IdentityKey].(string)
+	var user models.User
+	if result := db.Client.Preload("Roles.Permissions").Find(&user, userID); result.Error != nil {
+		logger.Error(result.Error)
+		c.Error(errs.ErrQueryCurrentUser)
+		return
+	}
+
+	var permissions = make([]string, 0)
+	for _, role := range user.Roles {
+		for _, permission := range role.Permissions {
+			permissions = append(permissions, permission.Name)
+		}
+	}
+
+	core.OK(c, permissions)
+}
+
 func (user *userController) Setup(r *gin.RouterGroup) {
 	r.GET("/users", user.QueryUsers).
 		POST("/user", user.CreateUser).
 		PUT("/user", user.UpdateUser).
 		DELETE("/user/:userID", user.DeleteUser).
-		GET("/user/current", user.QueryCurrentUser)
+		GET("/user/current", user.QueryCurrentUser).
+		GET("/user/current/permissions", user.QueryCurrentUserPermissions)
 }
