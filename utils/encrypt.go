fda04bbc9f2bac8f354fbe604cae52993e95edb8
ypli0629 <liyapeng0629@gmail.com>
2022-11-07 22:28:04 +0800

    feat: use pbkdf2

--- utils/encrypt.go
+++ utils/encrypt.go
@@ -1,12 +1,14 @@
 package utils
 
 import (
-	"crypto/md5"
+	"crypto/sha1"
 	"fmt"
+
+	"golang.org/x/crypto/pbkdf2"
 )
 
 // Encrypt encrypts plain text by md5sum
 func Encrypt(plainText string) string {
 	var val = []byte(plainText)
-	return fmt.Sprintf("%x", md5.Sum(val))
+	return fmt.Sprintf("%x", pbkdf2.Key(val, []byte("VglXSIH9WGEwkdB3"), 4096, 32, sha1.New))
 }
