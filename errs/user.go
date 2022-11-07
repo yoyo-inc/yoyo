887f2e5be89dd13611dda3d3e484bfd4df666549
ypli0629 <liyapeng0629@gmail.com>
2022-11-07 22:08:16 +0800

    feat: add user exist error

--- errs/user.go
+++ errs/user.go
@@ -3,7 +3,7 @@ package errs
 import "github.com/yoyo-inc/yoyo/core"
 
 var (
-	ErrFailedToLogin      = core.NewBusinessError("100", "登录失败")
+	ErrFailToLogin        = core.NewBusinessError("100", "登录失败")
 	ErrUsernameNotExists  = core.NewBusinessError("101", "账号不存在")
 	ErrUsernameOrPassword = core.NewBusinessError("102", "账号或密码错误")
 	ErrCreateUser         = core.NewBusinessError("103", "用户创建失败")
@@ -11,4 +11,5 @@ var (
 	ErrDeleteUser         = core.NewBusinessError("105", "用户删除失败")
 	ErrUpdateUser         = core.NewBusinessError("106", "用户更新失败")
 	ErrQueryCurrentUser   = core.NewBusinessError("107", "当前用户查询失败")
+	ErrExistUser          = core.NewBusinessError("108", "用户已存在")
 )
