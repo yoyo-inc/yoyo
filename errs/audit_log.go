e7ce597040a75fdf9af69e43bb340c833f9c5ab5
ypli0629 <liyapeng0629@gmail.com>
2022-11-14 10:37:37 +0800

    feat: add audit_log errs

--- errs/audit_log.go
+++ errs/audit_log.go
@@ -3,5 +3,6 @@ package errs
 import "github.com/yoyo-inc/yoyo/core"
 
 var (
-	ErrQueryAuditLog = core.NewBusinessError("300", "查询操作日志失败")
+	ErrQueryAuditLog       = core.NewBusinessError("300", "查询审计日志失败")
+	ErrQueryAuditLogModule = core.NewBusinessError("301", "查询审计日志模块失败")
 )
