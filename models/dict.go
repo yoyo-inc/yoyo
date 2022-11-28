01e6093e99d12297564f9f1bf7a2c25ba0ea3f1d
ypli0629 <liyapeng0629@gmail.com>
2022-11-28 10:52:27 +0800

    feat: add dict

---
+++ models/dict.go
@@ -0,0 +1,11 @@
+package models
+
+import "github.com/yoyo-inc/yoyo/core"
+
+type Dict struct {
+	core.Model
+	Type      string `json:"type" gorm:"comment:类型"`
+	Label     string `json:"label" gorm:"comment:标签"`
+	Value     string `json:"value" gorm:"comment:值"`
+	ValueType string `json:"valueType" gorm:"comment:值类型"`
+}
