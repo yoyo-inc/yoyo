4bd7e0e98ac6f58c250564b8e472d4a3daf15801
ypli0629 <liyapeng0629@gmail.com>
2022-11-30 16:31:49 +0800

    feat: add invalid parameters error

--- errs/common.go
+++ errs/common.go
@@ -3,5 +3,5 @@ package errs
 import "github.com/yoyo-inc/yoyo/core"
 
 var (
-	ErrInvalidParameters = core.NewBusinessError("001", "无效的参数")
+	ErrInvalidParameters = core.NewParameterError("无效的参数")
 )
