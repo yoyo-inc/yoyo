e2a42e768e64c57511f8b819846da1c385ce615a
ypli0629 <liyapeng0629@gmail.com>
2022-12-12 11:09:27 +0800

    feat: add download resource error

--- errs/resource.go
+++ errs/resource.go
@@ -7,4 +7,5 @@ var (
 	ErrUploadResource   = core.NewBusinessError("801", "上传资源文件失败")
 	ErrDeleteResource   = core.NewBusinessError("802", "删除资源文件失败")
 	ErrNotExistResource = core.NewBusinessError("803", "资源文件不存在")
+	ErrDownloadResource = core.NewBusinessError("804", "资源文件下载失败")
 )
