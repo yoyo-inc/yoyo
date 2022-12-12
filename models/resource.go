5f725abd34d5b7dd3327f5a8055e2358b5799040
ypli0629 <liyapeng0629@gmail.com>
2022-12-12 11:09:39 +0800

    feat: support resource type

--- models/resource.go
+++ models/resource.go
@@ -8,6 +8,7 @@ import (
 type Resource struct {
 	core.Model
 	ResourceName string  `json:"resourceName" gorm:"size:100;comment:资源名"`
+	ResourceType string  `json:"resourceType" gorm:"size:20;comment:资源类型"`
 	Filename     string  `json:"filename" gorm:"size:100;index;comment:原始文件名"`
 	Filesize     float64 `json:"filesize" gorm:"size:20;comment:文件大小"`
 	FileType     string  `json:"filetype" gorm:"size:20;comment:文件类型"`
