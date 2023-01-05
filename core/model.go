ae4b327910f671a110eda33d565536958f8ef7a7
ypli0629 <liyapeng0629@gmail.com>
2023-01-05 16:15:55 +0800

    feat: json tag add omitempty

--- core/model.go
+++ core/model.go
@@ -9,21 +9,15 @@ import (
 )
 
 type IModel struct {
-	// 主键
-	ID int `json:"id" gorm:"primarykey;autoIncreatment;comment:主键"`
-	// 创建时间
-	CreateTime *dt.LocalTime `json:"createTime" gorm:"type:timestamp;default:current_timestamp;<-:create;comment:创建时间"`
-	// 更新时间
-	ModifyTime *dt.LocalTime `json:"modifyTime" gorm:"type:timestamp;default:current_timestamp on update current_timestamp;comment:更新时间"`
+	ID         int           `json:"id,omitempty" gorm:"primarykey;autoIncreatment;comment:主键"`                                                     // 主键
+	CreateTime *dt.LocalTime `json:"createTime,omitempty" gorm:"type:timestamp;default:current_timestamp;<-:create;comment:创建时间"`                   // 创建时间
+	ModifyTime *dt.LocalTime `json:"modifyTime,omitempty" gorm:"type:timestamp;default:current_timestamp on update current_timestamp;comment:更新时间"` // 更新时间
 }
 
 type Model struct {
-	// 主键
-	ID string `json:"id" gorm:"primarykey;size:32;comment:主键"`
-	// 创建时间
-	CreateTime *dt.LocalTime `json:"createTime" gorm:"type:timestamp;default:current_timestamp;<-:create;comment:创建时间"`
-	// 更新时间
-	ModifyTime *dt.LocalTime `json:"modifyTime" gorm:"type:timestamp;default:current_timestamp on update current_timestamp;comment:更新时间"`
+	ID         string        `json:"id,omitempty" gorm:"primarykey;size:32;comment:主键"`                                                             // 主键
+	CreateTime *dt.LocalTime `json:"createTime,omitempty" gorm:"type:timestamp;default:current_timestamp;<-:create;comment:创建时间"`                   // 创建时间
+	ModifyTime *dt.LocalTime `json:"modifyTime,omitempty" gorm:"type:timestamp;default:current_timestamp on update current_timestamp;comment:更新时间"` // 更新时间
 }
 
 func (m *Model) BeforeCreate(tx *gorm.DB) (err error) {
