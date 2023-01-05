b7604e8f47390aaddd82b842f2896c29c9e7e43a
ypli0629 <liyapeng0629@gmail.com>
2023-01-05 16:15:10 +0800

    fix: filter by type

--- services/dict.go
+++ services/dict.go
@@ -14,14 +14,14 @@ type Entry struct {
 
 func GetEntriesByType(t string) ([]Entry, error) {
 	var dicts []models.Dict
-	if res := db.Client.Model(&models.Dict{}).Find(&dicts); res.Error != nil {
+	if res := db.Client.Model(&models.Dict{}).Where("type = ?", t).Find(&dicts); res.Error != nil {
 		return nil, res.Error
 	}
 
 	entries := make([]Entry, 0, len(dicts))
 	for _, dict := range dicts {
 		v, err := format(dict)
-		if err != nil {
+		if err == nil {
 			entries = append(entries, Entry{
 				Label: dict.Label,
 				Value: v,
@@ -34,7 +34,7 @@ func GetEntriesByType(t string) ([]Entry, error) {
 
 func GetLabelByValue(t string, v string) string {
 	var dict models.Dict
-	if res := db.Client.Where("value_type = ?", t).Where("value = ?", v).First(&dict); res.Error != nil {
+	if res := db.Client.Where("type = ?", t).Where("value = ?", v).First(&dict); res.Error != nil {
 		// return value when error raise
 		return v
 	}
