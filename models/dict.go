1e46c74bb2287e06d71d05e124b72062c6fa7609
ypli0629 <liyapeng0629@gmail.com>
2023-01-11 16:51:18 +0800

    fix: change field desc to description

--- models/dict.go
+++ models/dict.go
@@ -7,11 +7,11 @@ import (
 
 type Dict struct {
 	core.IModel
-	Type      string `json:"type,omitempty" gorm:"comment:类型"`
-	Desc      string `json:"desc,omitempty" gorm:"comment:描述"`
-	Label     string `json:"label,omitempty" gorm:"comment:标签"`
-	Value     string `json:"value,omitempty" gorm:"comment:值"`
-	ValueType string `json:"valueType,omitempty" gorm:"comment:值类型"`
+	Type        string `json:"type,omitempty" gorm:"comment:类型"`
+	Description string `json:"description,omitempty" gorm:"comment:描述"`
+	Label       string `json:"label,omitempty" gorm:"comment:标签"`
+	Value       string `json:"value,omitempty" gorm:"comment:值"`
+	ValueType   string `json:"valueType,omitempty" gorm:"comment:值类型"`
 }
 
 func init() {
