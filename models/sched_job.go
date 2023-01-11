41edc97405730a77f97f8bbee9e70e071ee608a7
ypli0629 <liyapeng0629@gmail.com>
2023-01-11 17:24:48 +0800

    fix: do not empty status field

--- models/sched_job.go
+++ models/sched_job.go
@@ -10,7 +10,7 @@ type SchedJob struct {
 	Type        string `json:"type,omitempty" gorm:"size:20;comment:任务类型"`
 	Spec        string `json:"spec,omitempty" gorm:"size:100;comment:规则"`
 	Description string `json:"description,omitempty" gorm:"size:100;comment:描述"`
-	Status      int    `json:"status,omitempty" gorm:"comment:是否启用0：不启用，1：启用"`
+	Status      int    `json:"status" gorm:"comment:是否启用0：不启用，1：启用"`
 	JobID       string `json:"jobID,omitempty" gorm:"comment:任务唯一标识"`
 	LastRunTime string `json:"lastRunTime,omitempty" gorm:"comment:上次运行时间"`
 }
