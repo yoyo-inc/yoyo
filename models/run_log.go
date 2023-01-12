ee579c570403581c5bd97535de12c9c6556135fe
ypli0629 <liyapeng0629@gmail.com>
2023-01-12 15:19:57 +0800

    feat: remove runlog model

--- models/run_log.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/yoyo-inc/yoyo/core"
-
-type RunLog struct {
-	core.IModel
-	Filename string `json:"filename,omitempty"`
-	Filesize string `json:"filesize,omitempty"`
-}
