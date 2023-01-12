68442194c6ae05fbe4f6209209381c48e5f48495
ypli0629 <liyapeng0629@gmail.com>
2023-01-12 15:20:39 +0800

    feat: add runlog

--- vo/run_log.go
+++ vo/run_log.go
@@ -1,5 +1,13 @@
 package vo
 
-type QueryRunLogVO struct {
-	Filename string `form:"filename"`
+type RunLogVO struct {
+	Service  string `json:"service,omitempty"`
+	Filename string `json:"filename,omitempty"`
+	Date     string `json:"date,omitempty"`
+	Filesize string `json:"filesize,omitempty"`
+}
+
+type DownloadRunLogVO struct {
+	Service  string `form:"service" binding:"required"`
+	Filename string `form:"filename" binding:"required"`
 }
