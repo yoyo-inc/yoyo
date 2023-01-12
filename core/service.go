66e1f3b07581c86222e8807f96a7a3250a698e44
ypli0629 <liyapeng0629@gmail.com>
2023-01-12 15:19:43 +0800

    feat: export GetParam

--- core/service.go
+++ core/service.go
@@ -9,8 +9,8 @@ import (
 )
 
 func Paginator(c *gin.Context) func(db *gorm.DB) *gorm.DB {
-	page, _ := strconv.Atoi(getParam(c, "current", "1"))
-	pageSize, _ := strconv.Atoi(getParam(c, "pageSize", "10"))
+	page, _ := strconv.Atoi(GetParam(c, "current", "1"))
+	pageSize, _ := strconv.Atoi(GetParam(c, "pageSize", "10"))
 
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Limit(pageSize).Offset((page - 1) * pageSize)
@@ -18,8 +18,8 @@ func Paginator(c *gin.Context) func(db *gorm.DB) *gorm.DB {
 }
 
 func DateTimeRanger(c *gin.Context, fieldName string) func(db *gorm.DB) *gorm.DB {
-	startTime := getParam(c, "startTime", "")
-	endTime := getParam(c, "endTime", "")
+	startTime := GetParam(c, "startTime", "")
+	endTime := GetParam(c, "endTime", "")
 
 	if startTime == "" || endTime == "" {
 		return func(db *gorm.DB) *gorm.DB {
@@ -42,7 +42,7 @@ func Orderer(c *gin.Context) func(db *gorm.DB) *gorm.DB {
 	}
 }
 
-func getParam(c *gin.Context, key string, defaultValue string) string {
+func GetParam(c *gin.Context, key string, defaultValue string) string {
 	result := defaultValue
 	if param, exists := c.GetQuery(key); exists {
 		result = param
