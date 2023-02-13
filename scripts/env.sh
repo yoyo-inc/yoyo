8b5c7b9109eec2c4723f1aa5b6291098bc8a03f1
ypli0629 <liyapeng0629@gmail.com>
2023-02-13 16:09:05 +0800

    chore: remove bind directory

--- scripts/env.sh
+++ scripts/env.sh
@@ -12,7 +12,7 @@ done
 docker run --name db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_DATABASE=yoyo -e MYSQL_USER=yoyo -e MYSQL_PASSWORD=yoyo \
 	-e TZ=Asia/Shanghai --restart=always -d mysql:8.0.28 --default-time_zone='+8:00'
 
-docker run --name prom -v /etc/hosts:/etc/hosts -v /etc/prometheus:/etc/prometheus -v /data/prom:/prometheus \
+docker run --name prom -v /etc/hosts:/etc/hosts -v /etc/prometheus:/etc/prometheus \
 	-p 9090:9090 --restart=always --network=host -d prom/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --storage.tsdb.retention=7d --web.enable-admin-api --web.enable-lifecycle --enable-feature=remote-write-receiver
 
 docker run --name alert -v /etc/hosts:/etc/hosts -v /etc/alertmanager:/etc/alertmanager -p 9093:9093 --network=host --restart=always -d prom/alertmanager
