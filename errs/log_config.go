2d701daada2984f14dbbd76c1121ad1d6bcb895f
ypli0629 <liyapeng0629@gmail.com>
2023-02-17 17:44:53 +0800

    fix: change log config

---
+++ errs/log_config.go
@@ -0,0 +1,5 @@
+package errs
+
+import "github.com/yoyo-inc/yoyo/core"
+
+var ErrUpdateLogConfig = core.NewBusinessError("1300", "更新日志设置失败")
--- routes/log_config.go
+++ routes/log_config.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"errors"
+	"github.com/yoyo-inc/yoyo/errs"
 	"path/filepath"
 
 	"github.com/gin-gonic/gin"
@@ -37,6 +38,7 @@ func (*logConfigController) QueryLogConfig(c *gin.Context) {
 }
 
 // SaveLogConfig
+//
 //	@Summary	保存日志配置
 //	@Tags		logConfig
 //	@Accept		json
@@ -47,36 +49,51 @@ func (*logConfigController) QueryLogConfig(c *gin.Context) {
 //	@Router		/log_config [post]
 func (lcc *logConfigController) SaveLogConfig(c *gin.Context) {
 	var logConfig models.LogConfig
+	if err := c.ShouldBindJSON(&logConfig); err != nil {
+		logger.Error(err)
+		c.Error(core.NewParameterError(err))
+		return
+	}
 
 	if logConfig.ID != "" {
 		if res := db.Client.Omit("id").Where("id = ?", logConfig.ID).Updates(&logConfig); res.Error != nil {
 			logger.Error(res.Error)
+			c.Error(errs.ErrUpdateLogConfig)
 			return
 		}
 	} else {
 		if res := db.Client.Create(&logConfig); res.Error != nil {
 			logger.Error(res.Error)
+			c.Error(errs.ErrUpdateLogConfig)
 			return
 		}
 	}
 
-	lcc.registerLogSchedJob()
+	err := lcc.registerLogSchedJob()
+	if err != nil {
+		logger.Error(err)
+		c.Error(errs.ErrUpdateLogConfig)
+		return
+	}
 
 	core.OK(c, true)
 }
 
-func (*logConfigController) registerLogSchedJob() {
+func (*logConfigController) registerLogSchedJob() error {
 	var logConfig models.LogConfig
 
 	if res := db.Client.Model(&models.LogConfig{}).First(&logConfig); res.Error != nil {
 		if !errors.Is(res.Error, gorm.ErrRecordNotFound) {
 			logger.Error(res.Error)
 		}
-		return
+		return nil
 	}
 
 	if logConfig.KeepTime != nil {
-		services.AddSchedJob("deleteLog", "log", "定时清除日志文件", "0 1 * * *", func() error {
+		if err := services.RemoveSchedJob("deleteLog"); err != nil {
+			return err
+		}
+		err := services.AddSchedJob("deleteLog", "log", "定时清除日志文件", "0 1 * * *", func() error {
 			srvs := config.GetMap("service_logs")
 			for k, v := range srvs {
 				p, err := filepath.Abs(v)
@@ -90,10 +107,20 @@ func (*logConfigController) registerLogSchedJob() {
 			}
 			return nil
 		})
+		if err != nil {
+			return err
+		}
+	} else {
+		if err := services.RemoveSchedJob("deleteLog"); err != nil {
+			return err
+		}
 	}
 
 	if logConfig.Archive {
-		services.AddSchedJob("archiveLog", "log", "定时归档日志文件", "0 1 * * *", func() error {
+		if err := services.RemoveSchedJob("archiveLog"); err != nil {
+			return err
+		}
+		err := services.AddSchedJob("archiveLog", "log", "定时归档日志文件", "0 1 * * *", func() error {
 			srvs := config.GetMap("service_logs")
 			for k, v := range srvs {
 				p, err := filepath.Abs(v)
@@ -107,7 +134,17 @@ func (*logConfigController) registerLogSchedJob() {
 			}
 			return nil
 		})
+		if err != nil {
+			return err
+		}
+	} else {
+		err := services.RemoveSchedJob("archiveLog")
+		if err != nil {
+			return err
+		}
 	}
+
+	return nil
 }
 
 func (lcc *logConfigController) Setup(r *gin.RouterGroup) {
