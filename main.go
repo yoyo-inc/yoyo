83c78ac724844c5003bdb2fe66ff16e4deaa189b
ypli0629 <liyapeng0629@gmail.com>
2023-04-23 22:51:35 +0800

    chore(fmt): format

--- main.go
+++ main.go
@@ -44,13 +44,13 @@ func ReadLoggerConfig() logger.Options {
 	return options
 }
 
-// @Title						yoyo
-// @Description				yoyo
-// @Host						127.0.0.1:8080
-// @BasePath					/API
-// @SecurityDefinitions.apikey	JWT
-// @In							header
-// @Name						Authorization
+//	@Title						yoyo
+//	@Description				yoyo
+//	@Host						127.0.0.1:8080
+//	@BasePath					/API
+//	@SecurityDefinitions.apikey	JWT
+//	@In							header
+//	@Name						Authorization
 func main() {
 	// config
 	config.Setup()
